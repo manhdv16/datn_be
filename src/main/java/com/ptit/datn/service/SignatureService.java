28c2296b39d5271a00c323d1eac75e14258dee9b
B20DCCN204 <giang41245@gmail.com>
2025-01-08 00:18:09 +0700

    update

--- src/main/java/com/ptit/datn/service/SignatureService.java
+++ src/main/java/com/ptit/datn/service/SignatureService.java
@@ -16,47 +16,36 @@ import java.util.Base64;
 public class SignatureService {
 
     public static String createSignature(String hash, String privateKeyBase64) throws Exception {
-        // Bước 1: Giải mã private key từ base64
         byte[] privateKeyBytes = Base64.getDecoder().decode(privateKeyBase64);
 
-        // Bước 2: Chuyển đổi byte[] thành PrivateKey
         PKCS8EncodedKeySpec keySpec = new PKCS8EncodedKeySpec(privateKeyBytes);
         KeyFactory keyFactory = KeyFactory.getInstance("RSA");
         PrivateKey privateKey = keyFactory.generatePrivate(keySpec);
 
-        // Bước 1: Khởi tạo Signature với thuật toán SHA256withRSA
         Signature signature = Signature.getInstance("SHA256withRSA");
         signature.initSign(privateKey);
 
-        // Bước 2: Cung cấp dữ liệu cần ký (hash) dưới dạng byte
         byte[] hashBytes = hash.getBytes("UTF-8");
         signature.update(hashBytes);
 
-        // Bước 3: Ký dữ liệu
         byte[] signatureBytes = signature.sign();
 
-        // Bước 4: Chuyển chữ ký sang định dạng Base64
         return Base64.getEncoder().encodeToString(signatureBytes);
     }
 
 
     public static boolean verifySignature(String data, String signature, String publicKeyBase64) {
         try {
-            //Lấy Public Key từ chuỗi Base64 (lưu trong DB)
             byte[] publicKeyBytes = Base64.getDecoder().decode(publicKeyBase64);
             KeyFactory keyFactory = KeyFactory.getInstance("RSA");
             PublicKey publicKey = keyFactory.generatePublic(new X509EncodedKeySpec(publicKeyBytes));
 
-            //Tạo đối tượng Signature với thuật toán SHA256withRSA
             Signature rsaSignature = Signature.getInstance("SHA256withRSA");
 
-            //Cấu hình Public Key để xác thực
             rsaSignature.initVerify(publicKey);
 
-            //Cập nhật dữ liệu gốc
             rsaSignature.update(data.getBytes());
 
-            //Giải mã và xác thực chữ ký
             byte[] signatureBytes = Base64.getDecoder().decode(signature);
             return rsaSignature.verify(signatureBytes);
         } catch (Exception e) {
